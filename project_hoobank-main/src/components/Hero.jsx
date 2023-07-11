f544dba2264c89b469b519081a199254d59531f1
aapsi <aapsi0001@gmail.com>
2023-07-12 03:40:55 +0530

    Hero-GetStarted-component

--- project_hoobank-main/src/components/Hero.jsx
+++ project_hoobank-main/src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { discount, robot } from '../assets';
 import styles from "../style";
+import GetStarted from "./GetStarted";
 
 
 const Hero = () => {
@@ -9,33 +10,56 @@ const Hero = () => {
     // use id=home as we want to add scroll and we can control DOM elements through id
 
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
-      <div className={`flex-1 ${styles.flexStart}
-      flex-col xl:px-0 sm:px-16 px-6`}>
-        <div className='flex flex-row
-        items-center py-[6px] px-4
-        bg-discount-gradient rounded-[10px] mb-2'>
-          <img src={discount} alt='discount'
-          className='w-[32px] h-[32px]'/>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
           <p className={`${styles.paragraph} ml-2`}>
-            <span className='text-white'>20%</span> Discount For {" "}
-            <span className='text-white'>1 Month</span> Account 
+            <span className="text-white">20%</span> Discount For{" "}
+            <span className="text-white">1 Month</span> Account
           </p>
         </div>
 
         <div className='flex flex-row
-        justify-between items-center w-4'>
+        justify-between items-center w-full'>
 {/* Use flex-1 to allow a flex item to grow and shrink as needed, ignoring its initial size  */}
 
           <h1 className='flex-1 font-poppins
-          font-semibold ss:text-[72px] text-[52px] text-white'>
+          font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>
             The Next <br className='sm:block
             hidden'/> {" "}
             <span className='text-gradient'>Generation</span> {" "}
-            Payment Method.
           </h1>
-        
-        </div>  
+          <div className='ss:flex hidden md:mr-4 mr-0'>
+            <GetStarted/>
+          </div>
+        </div>
+        <h1 className='font-poppins
+        font-semibold ss:text-[68px] text-[52px]
+        text-white ss:leading-[100px] leading-[75px] w-full'>
+        Payment Method.</h1>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Our team of experts uses a methodology to identify
+        the credit cards most likely to fit your needs. 
+        We examine annual percentage rates, annual fees.</p>
+ 
       </div>
+
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+        {/* z-[5] because we wan tout robot image to appear on top of gradient */}
+        <img src={robot} alt="billing" className='w-[100%]
+        relative z-[5]'/>
+
+        {/* gradient start */}
+        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+        {/* gradient end */}
+
+      </div>
+
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted/>
+      </div>
+
     </section>
   )
 }
